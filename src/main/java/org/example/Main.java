a1d3433cd9c6c71009d800e4205b45f6329820c0
zyh02319 <138550509+zyh02319@users.noreply.github.com>
2025-02-24 14:36:36 +0800

    Update Main.java
    修改了6次

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,6 +7,7 @@ public class Main {
         System.out.println("修改了四次");
         System.out.println("修改了三次");
         System.out.println("修改了五次");
+        System.out.println("修改了六次");
         System.out.println(add(1, 2));
     }
     public static int add(int a, int b) {
