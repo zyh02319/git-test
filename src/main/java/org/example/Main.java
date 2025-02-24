92b0b7d3ed5f167f56ba094af59a54eb0e677af5
zyh02319 <2365174401@qq.com>
2025-02-24 14:07:38 +0800

    修改提交

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println(add(1, 2));
+        System.out.println("修改了");
     }
     public static int add(int a, int b) {
         return a + b;
