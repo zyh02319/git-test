ea38d54c6923336d380d3d42afd5c1b74935b1c5
zyh02319 <2365174401@qq.com>
2025-02-24 14:08:44 +0800

    修改提交1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("修改了两次");
         System.out.println(add(1, 2));
     }
     public static int add(int a, int b) {
