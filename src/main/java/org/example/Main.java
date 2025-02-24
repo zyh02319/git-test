fbac0af154ba30211d2e6ecdc502bba70871bce3
zyh02319 <2365174401@qq.com>
2025-02-24 12:38:04 +0800

    第三次提交

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,8 +3,11 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println(add(1, 2));
     }
     public static int add(int a, int b) {
         return a + b;
     }
+
+
 }
