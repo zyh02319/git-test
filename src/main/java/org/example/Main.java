f8df68dfcccdcf56b8aafc2b714f86b779f60c6b
zyh02319 <2365174401@qq.com>
2025-02-24 14:13:19 +0800

    修改提交2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("修改了两次");
+        System.out.println("修改了三次");
         System.out.println(add(1, 2));
     }
     public static int add(int a, int b) {
